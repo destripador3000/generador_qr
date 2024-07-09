539c06a7d5b9be8be5a7981ea66575279e3738a9
Juan Esteban <114034675+destripador3000@users.noreply.github.com>
2024-07-09 02:09:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import qrcode
-sitio = 'https://www.google.com/maps/place/HELADOS+CHELA/@4.2663322,-75.9311091,15z/data=!4m6!3m5!1s0x8e384dff1641e395:0xa850caeedfaea541!8m2!3d4.2663322!4d-75.9311091!16s%2Fg%2F11r3tqtgv_?entry=ttu'
-
-qr = qrcode.QRCode(version=1,box_size=10,border=5)
+sitio = input("Coloca la url que quieras convertir a qr")
 qr.add_data(sitio)
 qr.make(fit=True)
 img =qr.make_image(fill='black', back_color='white')
