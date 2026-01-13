d1e0b6c9c37b8e417e71bb30826eda18db32b759
Juan Esteban Valencia Londoño <114034675+destripador3000@users.noreply.github.com>
2026-01-12 20:34:09 -0500

    Refactor QR code generation and improve user prompts

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 import qrcode
-sitio = input("Coloca la url que quieras convertir a qr")
+
+sitio = input("Coloca la URL que quieras convertir a QR: ")
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+
 qr.add_data(sitio)
 qr.make(fit=True)
-img =qr.make_image(fill='black', back_color='white')
-img.save('heladoschela.png')
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("heladoschela.png")
+
+print("QR generado correctamente como <nombreDefotoResultante>.png")
+
